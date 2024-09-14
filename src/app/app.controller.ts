dbb024ab5d9055789671ba4f07d4fb170844af05
pmfrp <pmfrp@hotmail.com>
2024-09-14 11:25:35 -0300

    controllers

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('home')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('exemplo')
+  getExemplo(): string {
+    return 'exemplo';
   }
 }
